09f129543b075e11b92b00699df35271f3c0f3fb
Young <857531530@qq.com>
2019-08-06 15:51:39 +0800

    feat: 🎸 add routes for future use

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { Route } from 'react-router-dom';
+// import Summit from '@pages/summit/App.jsx';
+
+// export default (
+//   <Route path="/" component={Summit}>
+//     {/* <Route path='*' component={NoMatch} /> */}
+//   </Route>
+// );
