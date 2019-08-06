6d01822b5d24dd80f8d7fbca7941f8ca54647b56
atom-young <atom.yang@hoopox.com>
2019-08-06 14:12:15 +0800

    feat: 🎸 add react-bootstrap

--- src/pages/summit/App.jsx
+++ src/pages/summit/App.jsx
@@ -3,12 +3,15 @@
  * @author atom-yang
  */
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import './index.less';
+import 'bootstrap/dist/css/bootstrap.css';
 
 const app = () => (
   <div>
     <div className="index-container">
       大会首页
+      <Button variant="primary">Primary</Button>
     </div>
   </div>
 );
