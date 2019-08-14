1a18ff16a668365561ad8491f67163cdfe1c7a04
Alfred Yang <alfred.yang@aelf.io>
2019-08-14 20:20:47 +0800

    perf: ⚡️ add React-Snap to pre-render the project

--- src/pages/summit/index.jsx
+++ src/pages/summit/index.jsx
@@ -3,12 +3,19 @@
  * @author atom-yang
  */
 import React from 'react';
-import { render } from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import App from './App';
-import Repairing from './Repairing';
+// import Repairing from './Repairing';
 
-const isProdMode = process.env.NODE_ENV === 'production';
-const Element = isProdMode ? Repairing : App;
+// const isProdMode = process.env.NODE_ENV === 'production';
+// const Element = isProdMode ? Repairing : App;
+
+const rootElement = document.getElementById('app');
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
 // 这是编译的做法，如果项目要上线的话还要重新编译
 // 可以整两个page，修改nginx的路径
-render(<Element />, document.getElementById('app'));
