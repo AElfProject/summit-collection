d21eb6de18d93b41ddc8e1e2020c0871a22283b8
Alfred Yang <alfred.yang@aelf.io>
2019-08-16 16:44:23 +0800

    fix: 🐛 a bug of config

--- src/pages/Repairing/index.jsx
+++ src/pages/Repairing/index.jsx
@@ -1,17 +1,11 @@
+/**
+ * @file entry point
+ * @author atom-yang
+ */
 import React from 'react';
-import 'minireset.css/minireset.min.css';
-import './index.less';
-import CopyRight from '@components/CopyRight/';
+import { render } from 'react-dom';
+import App from './App';
 
-function Repairing() {
-  return (
-    <div className="repairing-container">
-      <div className="bg-img-container" />
-      <h1 className="page-title">页面正在进行升级维护:)</h1>
-      <p className="tips">请等会儿再来瞅瞅吧</p>
-      <CopyRight style={{ color: '#fff' }} />
-    </div>
-  );
-}
+const rootElement = document.getElementById('app');
 
-export default Repairing;
+render(<App />, rootElement);
